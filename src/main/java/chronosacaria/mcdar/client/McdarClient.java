a2b1af6ead1cbcbd32893fd59bb71bf35aa8764b
chronosacaria <chronos.droid@gmail.com>
2024-04-09 11:42:13 -0400

    Added Optional 3D Resource Pack by Lemo  - Added 3D MCDAR as an optional resource pack until its completion, at which time it will become the default textures and models

--- src/main/java/chronosacaria/mcdar/client/McdarClient.java
+++ src/main/java/chronosacaria/mcdar/client/McdarClient.java
@@ -1,15 +1,35 @@
 package chronosacaria.mcdar.client;
 
+import chronosacaria.mcdar.Mcdar;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.util.Identifier;
+
 
 @Environment(EnvType.CLIENT)
 public class McdarClient implements ClientModInitializer {
+    @SuppressWarnings({"deprecation", "Note: ResourceManagerHelper.registerBuiltinResourcePack isn't actually deprecated, but overloaded."})
+    private static void loadOptionalResourcePack(ModContainer modContainer) {
+        ResourceManagerHelper.registerBuiltinResourcePack(
+                new Identifier(Mcdar.MOD_ID, "3dmcdar"),
+                modContainer,
+                "3D MCDAR",
+                ResourcePackActivationType.NORMAL);
+    }
+
     @Override
     public void onInitializeClient() {
 
         //SummonedEntityRenderRegistry.register();
         SummonedEntityRenderRegistry.init();
+
+        // Optional Resource Pack
+        FabricLoader.getInstance().getModContainer(Mcdar.MOD_ID).ifPresent(
+                McdarClient::loadOptionalResourcePack);
     }
 }
