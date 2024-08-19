eaef0d9922c2e169c2bd1cbd16d6f14c33d3dc51
chronosacaria <chronos.droid@gmail.com>
2024-08-19 00:46:30 -0400

    Resolved Crash, Seemingly fixed #41, Version Bump  - Resolved Crash related to trying to cast `ClientWorld` to `ServerWorld`  - Resolving this appears to have resolved the issue present in Issue #41

--- src/main/java/chronosacaria/mcdar/artifacts/SatchelOfElementsItem.java
+++ src/main/java/chronosacaria/mcdar/artifacts/SatchelOfElementsItem.java
@@ -6,12 +6,12 @@ import chronosacaria.mcdar.api.CleanlinessHelper;
 import chronosacaria.mcdar.api.McdarEnchantmentHelper;
 import chronosacaria.mcdar.enums.StatusInflictingArtifactID;
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -25,22 +25,30 @@ public class SatchelOfElementsItem extends ArtifactStatusInflictingItem{
         );
     }
 
-    public TypedActionResult<ItemStack> use (World world, PlayerEntity user, Hand hand){
-        ItemStack itemStack = user.getStackInHand(hand);
+    @Override
+    public ActionResult useOnBlock (ItemUsageContext context) {
+        if (context.getPlayer() != null && context.getPlayer().getWorld().getServer() != null) {
+            ServerPlayerEntity user = context.getPlayer().getWorld().getServer().getPlayerManager().getPlayer(context.getPlayer().getUuid());
+            if (user != null) {
+                Hand hand = user.getActiveHand();
+                ItemStack itemStack = user.getStackInHand(hand);
 
-        if (user.totalExperience >= 15 || user.isCreative()) {
-            AOEHelper.satchelOfElementsEffects(user);
+                if (user.totalExperience >= 15 || user.isCreative()) {
+                    AOEHelper.satchelOfElementsEffects(user);
 
-            if (!user.isCreative()) {
-                user.addExperience(-15);
-                itemStack.damage(1, user, (entity) -> entity.sendToolBreakStatus(hand));
+                    if (!user.isCreative()) {
+                        user.addExperience(-15);
+                        itemStack.damage(1, user, (entity) -> entity.sendToolBreakStatus(hand));
+                    }
+                    McdarEnchantmentHelper.mcdar$cooldownHelper(
+                            user,
+                            this
+                    );
+                    return ActionResult.CONSUME;
+                }
             }
-            McdarEnchantmentHelper.mcdar$cooldownHelper(
-                    user,
-                    this
-            );
         }
-        return new TypedActionResult<>(ActionResult.SUCCESS, itemStack);
+        return ActionResult.PASS;
     }
 
     @Override
